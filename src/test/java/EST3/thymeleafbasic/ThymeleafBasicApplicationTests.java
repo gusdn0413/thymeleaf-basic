8b9ac81ced4274c6cc64ec0f7d28c8fbd64f1701
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2023-09-08 22:09:15 +0900

    Initial commit

---
+++ src/test/java/EST3/thymeleafbasic/ThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package EST3.thymeleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
